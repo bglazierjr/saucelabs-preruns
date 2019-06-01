3b97045e89d3d0ae7e8d18de79f09a238db7c058
502492634 <Bruce.Grazier1@syf.com>
2019-06-01 17:09:47 -0400

    Safari preconfig

---
+++ safaritabs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool true
